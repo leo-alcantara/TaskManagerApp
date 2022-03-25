33fd68c4bef465e547d6af135a9fd22468bbe5e6
leoodindealcantara <leoalcantara@icloud.com>
2022-03-25 12:40:51 +0100

    Last Commit

--- src/main/java/spp/tasksloggingapi/Exceptions/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package spp.tasksloggingapi.Exceptions;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException (String message){
-        super(message);
-    }
-}
